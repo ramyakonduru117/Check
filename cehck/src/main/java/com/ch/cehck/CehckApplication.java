85d3139c826e2d3976590f3e480278579e30f836
Konduru Ramya <1480@modus.local1>
2025-09-10 12:10:14 +0530

    first commit

---
+++ cehck/src/main/java/com/ch/cehck/CehckApplication.java
@@ -0,0 +1,13 @@
+package com.ch.cehck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CehckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CehckApplication.class, args);
+	}
+
+}
---
+++ cehck/src/test/java/com/ch/cehck/CehckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ch.cehck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CehckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
