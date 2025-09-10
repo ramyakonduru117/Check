3d93df34e008fe214fd531efed71f99d690c0eb9
Konduru Ramya <1480@modus.local1>
2025-09-10 17:47:51 +0530

    sjkdhjk

--- cehck/src/main/java/com/ch/cehck/controller/HelloController.java
+++ cehck/src/main/java/com/ch/cehck/controller/HelloController.java
@@ -1,4 +1,12 @@
 package com.ch.cehck.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    public  void to(){
+        //version 1
+        System.out.println("sdjklk");
+    }
 }
